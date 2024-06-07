3646c5c3bf838d323d9a536a694dd4224d8b8196
SeungYeon <wjdac66@gmail.com>
2024-06-07 21:56:01 +0900

    [refactor] Add header comments and fix include guard in common.h
    - Added descriptive comments at the top of the common.h file to explain its purpose.
    - Changed #ifdef to #ifndef for the include guard to prevent multiple inclusions.

--- common.h
+++ common.h
@@ -1,8 +1,11 @@
-#ifdef clox_common_h
+// common.h
+// This file defines common types and constants used throughout the interpreter.
+
+#ifndef clox_common_h
 #define clox_common_h
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-#endif
+#endif // clox_common_h
