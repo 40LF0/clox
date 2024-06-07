3fe5bab4ea772280b770e1ce4edf5a13a5e94353
SeungYeon <wjdac66@gmail.com>
2024-06-07 23:09:07 +0900

    [feat] Implement chunk module for bytecode representation
    - Added chuck.h file to define a chunk
    - Added chuck.c file to implement functions for initializing, freeing, and writing bytecode chunks.
    - Implemented functions initChunk(), freeChunk(), and writeChunk() to manage bytecode chunks.

---
+++ chuck.c
@@ -0,0 +1,33 @@
+// chuck.c
+// Created by Seungyeon Chung on 2024.06.07.
+//
+// This module defines a chunk, which is a sequence of bytecode instructions
+// used to represent code in the interpreter.
+
+#include <stdlib.h>
+
+#include "chuck.h"
+#include "memory.h"
+
+void initChunk(Chunk *chunk) {
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+}
+
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte) {
+  if (chunk->capacity < chunk->count + 1) {
+    int oldCapacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
--- chuck.h
+++ chuck.h
@@ -7,9 +7,21 @@
 #ifndef CLOX_CHUCK_H
 #define CLOX_CHUCK_H
 
+#include "common.h"
+
 // In the bytecode format, each instruction consists of a 1-byte operation code.
 // This number represents the type of instruction,
 // such as addition, subtraction, variable lookup, etc.
 typedef enum { OP_RETURN } OpCode;
+
+typedef struct {
+  int count;         // Number of bytes in the chunk
+  int capacity;      // Capacity of the chunk (allocated memory)
+  uint8_t* code;     // Array to store bytecodes
+} Chunk;
+
+void initChunk(Chunk* chuck);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif // CLOX_CHUCK_H
