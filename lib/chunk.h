1ab810bf81386e40afda0ee1ffc22d348c254011
SeungYeon <wjdac66@gmail.com>
2024-06-09 00:15:35 +0900

    [Feat] Add immediate instruction to chunk module - Chunk now has constants(ValueArray) - Add OP_CONSTANT to opcode - Implement add value function to chunk

--- lib/chunk.h
+++ lib/chunk.h
@@ -8,20 +8,23 @@
 #define CLOX_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 // In the bytecode format, each instruction consists of a 1-byte operation code.
 // This number represents the type of instruction,
 // such as addition, subtraction, variable lookup, etc.
-typedef enum { OP_RETURN } OpCode;
+typedef enum { OP_CONSTANT, OP_RETURN } OpCode;
 
 typedef struct {
-  int count;         // Number of bytes in the chunk
-  int capacity;      // Capacity of the chunk (allocated memory)
-  uint8_t* code;     // Array to store bytecodes
+  int count;            // Number of bytes in the chunk
+  int capacity;         // Capacity of the chunk (allocated memory)
+  uint8_t *code;        // Array to store bytecodes
+  ValueArray constants; // Array to store values
 } Chunk;
 
-void initChunk(Chunk* chuck);
-void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void initChunk(Chunk *chuck);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
+int addConstant(Chunk *chunk, Value value);
 
 #endif // CLOX_CHUNK_H
