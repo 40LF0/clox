3b2c72035e0b717768d78f8d3767b53a9222e349
SeungYeon <wjdac66@gmail.com>
2024-06-09 00:06:52 +0900

    [Feat] Add value module for represent literal constant. - Define "Value" and "ValueArray" - Implement init, write, free functions for manage ValueArray. - Implement print function for Value.

---
+++ lib/value.h
@@ -0,0 +1,25 @@
+// value.h
+// Created by Seungyeon Chung on 2024.06.08.
+//
+// This module defines a value, which is literal constant.
+// The immediate instruction following the opcode points to this value.
+
+#ifndef CLOX_VALUE_H
+#define CLOX_VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+  int capacity;  // Capacity of the valueArray (allocated memory)
+  int count;     // Number of values in the valueArray
+  Value *values; // Array to store values
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif // CLOX_VALUE_H
