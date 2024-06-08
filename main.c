660ecf212054d76ca12f361ad7466fef19bc2db9
SeungYeon <wjdac66@gmail.com>
2024-06-08 11:53:41 +0900

    [feat] Verify chunk and debug modules in main function
    - Implement main function to initialize and test bytecode chunk
    - Integrate debug module to disassemble and print bytecode instructions
    - Ensure proper initialization and cleanup of Chunk structure

--- main.c
+++ main.c
@@ -3,8 +3,15 @@
 //
 // Entry point for the interpreter. This file contains the main function.
 
-#include "lib/common.h"
+#include "common.h"
+#include "debug.h"
 
 int main(int argc, const char *argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
   return 0;
 }
