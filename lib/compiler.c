dc92008a58b8b36f1c47779eba461210c1ebc689
SeungYeon <wjdac66@gmail.com>
2024-06-14 12:32:08 +0900

    [feat] create scanner and connect to pipeline - Define the TokenType enumeration to represent token type - Define Token - Create initial scanToken method - Implement temporary compile function to check scanner operation

--- lib/compiler.c
+++ lib/compiler.c
@@ -2,10 +2,26 @@
 // Created by Seungyeon Chung on 2024.06.12.
 // A compiler is a component that generate bytecode chunk from tokens.
 
-#include "common.h"
 #include "compiler.h"
+
+#include <stdio.h>
+
+#include "common.h"
 #include "scanner.h"
 
 void compile(const char *source) {
   initScanner(source);
+  int line = -1;
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("   | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF) break;
+  }
 }
