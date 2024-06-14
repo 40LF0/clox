90162d12af62ceb7820bd5315913821b00de56ff
SeungYeon <wjdac66@gmail.com>
2024-06-14 11:52:43 +0900

    [feat] create scanner and connect to pipeline - implement scanner structure - implement init scanner method - connect scanner module to compiler module

--- lib/compiler.c
+++ lib/compiler.c
@@ -2,6 +2,10 @@
 // Created by Seungyeon Chung on 2024.06.12.
 // A compiler is a component that generate bytecode chunk from tokens.
 
+#include "common.h"
 #include "compiler.h"
+#include "scanner.h"
 
-void compile(const char *source) {}
+void compile(const char *source) {
+  initScanner(source);
+}
