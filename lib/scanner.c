1d1a5264de0ddfcfa1d905c5b5f6c489a42b2ea2
SeungYeon <wjdac66@gmail.com>
2024-06-14 11:52:43 +0900

    [feat] create scanner and connect to pipeline - implement scanner structure - implement init scanner method - connect scanner module to compiler module

---
+++ lib/scanner.c
@@ -0,0 +1,24 @@
+// scanner.c
+// Created by Seungyeon Chung on 2024.06.14.
+//
+
+#include "scanner.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+
+typedef struct {
+  const char* start;
+  const char* current;
+  int line;
+} Scanner;
+
+Scanner scanner;
+
+void initScanner(const char* source) {
+  scanner.start = source;
+  scanner.current = source;
+  scanner.line = 1;
+}
---
+++ lib/scanner.h
@@ -0,0 +1,10 @@
+// scanner.h
+// Created by Seungyeon Chung on 2024.06.14.
+//
+
+#ifndef CLOX_SCANNER_H
+#define CLOX_SCANNER_H
+
+void initScanner(const char* source);
+
+#endif  // CLOX_SCANNER_H
