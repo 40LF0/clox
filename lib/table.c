35652d84db5c5379b11d42651948b84052b36b88
Seungyeon Chung <wjdac66@gmail.com>
2024-07-03 00:23:43 +0900

    [feat] define Table and Entry structure for hash table module. - implement init hash table method. - implement fre hash table method.

--- lib/table.c
+++ lib/table.c
@@ -4,3 +4,21 @@
 // This module defines and implements a hash table.
 
 #include "table.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+
+void initTable(Table* table) {
+  table->count = 0;
+  table->capacity = 0;
+  table->entries = NULL;
+}
+
+void freeTable(Table* table) {
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  initTable(table);
+}
--- lib/table.h
+++ lib/table.h
@@ -6,6 +6,21 @@
 #ifndef CLOX_TABLE_H
 #define CLOX_TABLE_H
 
+#include "common.h"
+#include "value.h"
 
+typedef struct {
+  ObjString* key;
+  Value value;
+} Entry;
 
-#endif //CLOX_TABLE_H
+typedef struct {
+  int count;
+  int capacity;
+  Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+
+#endif  // CLOX_TABLE_H
