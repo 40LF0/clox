fceeabff636bb53731fc1b33d55bbe13fe385311
Seungyeon Chung <wjdac66@gmail.com>
2024-07-04 02:14:46 +0900

    [feat] utility methods for hash table module. - implement method for searching value with key in the hash table. - implement method for deleting entry in the hash table using tombstone. - update findEntry, tableSet, adjustCapacity methods to reflect tombstone.

--- lib/table.c
+++ lib/table.c
@@ -28,14 +28,36 @@ void freeTable(Table* table) {
 // Linear Probing for open addressing.
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
   uint32_t index = key->hash % capacity;
+  Entry* tombstone = NULL;
   for (;;) {
     Entry* entry = &entries[index];
-    if (entry->key == key || entry->key == NULL) {
+
+    if (entry->key == key) {
       return entry;
+    }
+
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      }
+
+      if (tombstone == NULL) {
+        tombstone = entry;
+      }
     }
 
     index = (index + 1) % capacity;
   }
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+  if (table->count == 0) return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  *value = entry->value;
+  return true;
 }
 
 static void adjustCapacity(Table* table, int capacity) {
@@ -45,6 +67,7 @@ static void adjustCapacity(Table* table, int capacity) {
     entries[i].value = NIL_VAL;
   }
 
+  table->count = 0;
   for (int i = 0; i < table->capacity; i++) {
     Entry* entry = &table->entries[i];
     if (entry->key == NULL) continue;
@@ -52,6 +75,7 @@ static void adjustCapacity(Table* table, int capacity) {
     Entry* dest = findEntry(entries, capacity, entry->key);
     dest->key = entry->key;
     dest->value = entry->value;
+    table->count++;
   }
 
   FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -62,16 +86,26 @@ static void adjustCapacity(Table* table, int capacity) {
 bool tableSet(Table* table, ObjString* key, Value value) {
   if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
     int capacity = GROW_CAPACITY(table->capacity);
-    adjustCapacity(table,capacity);
+    adjustCapacity(table, capacity);
   }
 
   Entry* entry = findEntry(table->entries, table->capacity, key);
   bool isNewKey = entry->key == NULL;
-  if (isNewKey) table->count++;
+  if (isNewKey && IS_NIL(entry->value)) table->count++;
 
   entry->key = key;
   entry->value = value;
   return isNewKey;
+}
+bool tableDelete(Table* table, ObjString* key) {
+  if (table->count == 0) return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  entry->key = NULL;
+  entry->value = BOOL_VAL(true);
+  return true;
 }
 
 void tableAddAll(Table* from, Table* to) {
