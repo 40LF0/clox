c774d184f7714dd048b1a10dc778ffdaf255c74c
Seungyeon Chung <wjdac66@gmail.com>
2024-07-16 00:19:05 +0900

    [feat] update compiler module for function object.

--- lib/compiler.c
+++ lib/compiler.c
@@ -47,12 +47,20 @@ typedef struct {
   Precedence precedence;
 } ParseRule;
 
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT,
+} FunctionType;
+
 typedef struct {
   Token name;
   int depth;
 } Local;
 
 typedef struct {
+  ObjFunction* function;
+  FunctionType type;
+
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
@@ -60,9 +68,8 @@ typedef struct {
 
 Parser parser;
 Compiler* current = NULL;
-Chunk* compilingChunk;
 
-static Chunk* currentChunk() { return compilingChunk; }
+static Chunk* currentChunk() { return &current->function->chunk; }
 
 static void errorAt(Token* token, const char* message) {
   if (parser.panicMode) return;
@@ -167,19 +174,33 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
+
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currentChunk(), function->name != NULL
+                                         ? function->name->chars
+                                         : "<script>");
   }
 #endif
+
+  return function;
 }
 
 static void beginScope() { current->scopeDepth++; }
@@ -536,7 +557,8 @@ static void switchStatement() {
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
   consume(TOKEN_LEFT_BRACE, "Expect '{' before switch cases.");
 
-  int caseEndJumps[MAX_SWITCH_CASES];  // To store jumps from cases to end of switch
+  int caseEndJumps[MAX_SWITCH_CASES];  // To store jumps from cases to end of
+                                       // switch
   int caseCount = 0;
 
   // Loop through cases and the default block
@@ -703,11 +725,10 @@ static void statement() {
   }
 }
 
-bool compile(const char* source, Chunk* chunk) {
+ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -716,6 +737,6 @@ bool compile(const char* source, Chunk* chunk) {
   while (!match(TOKEN_EOF)) {
     declaration();
   }
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction* function = endCompiler();
+  return parser.hadError ? NULL : function;
 }
