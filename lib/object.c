967001c06013c27b592359eca198f980b61e7186
Seungyeon Chung <wjdac66@gmail.com>
2024-08-04 00:04:40 +0900

    [feat] add class object feature to object module.

--- lib/object.c
+++ lib/object.c
@@ -54,6 +54,12 @@ static uint32_t hashString(const char* key, int length) {
   return hash;
 }
 
+ObjClass* newClass(ObjClass* name) {
+  ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+  klass->name = name;
+  return klass;
+}
+
 ObjClosure* newClosure(ObjFunction* function) {
   ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
   for (int i = 0; i < function->upvalueCount; i++) {
@@ -120,6 +126,9 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
   switch (OBJ_TPYE(value)) {
+    case OBJ_CLASS:
+      printf("%s", AS_CLASS(value)->name->chars);
+      break;
     case OBJ_CLOSURE:
       printFunction(AS_CLOSURE(value)->function);
       break;
